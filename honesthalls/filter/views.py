4a3065695ed5ef7766e5a55037679cdcdadc71bf
Hisham Alyahya <hishamaalyahya@gmail.com>
2020-02-05 14:15:59 +0000

    Started filter page

--- honesthalls/filter/views.py
+++ honesthalls/filter/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from .forms import FilterForm
 
 # Create your views here.
 def filter_view(request):
-    return render(request, 'filter/form.html')
+    halls = [
+        {
+            'name': 'Hall1',
+            'isEnsuite': True
+        },
+        {
+            'name': 'Hall2',
+            'isEnsuite': False
+        }
+    ]
+    submitted = request.POST
+    isEnsuite = None
+    if(submitted):
+        form = FilterForm(request.POST)
+        if(form.is_valid()):
+            isEnsuite = form.cleaned_data.get('isEnsuite')
+    else:
+        form = FilterForm()
+    context = {
+        'form': form,   #
+        'halls': halls,
+        'isEnsuite': isEnsuite, 
+        'submitted': submitted
+    }
+    return render(request, 'filter/form.html', context)
