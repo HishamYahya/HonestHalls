0a4bb5e050c716faa563938a6b19b24717c77bbb
Gabrielle Mannifield <gabrielle.mannifield@student.manchester.ac.uk>
2020-02-06 19:57:12 +0000

    'made_halls_page'

--- honesthalls/halls/views.py
+++ honesthalls/halls/views.py
@@ -5,3 +5,16 @@ from django.utils import timezone
 def index(request):
     context = { 'server_time': timezone.now() }
     return render(request, 'halls/index.html', context)
+
+
+def hallpage(request):
+	# For fully working version, we need to get this info from database
+	context = {
+		'title': 'Hall Page',
+		'hallname': 'Hulme Hall',
+		'campus': 'Victoria Park',
+		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at eros porta elit faucibus luctus. Maecenas justo massa, euismod in pulvinar in, accumsan a erat. Sed ac dui ipsum. Nulla dapibus viverra rutrum. Proin id felis at massa ornare tristique. Morbi turpis mauris, facilisis non dolor vel, gravida scelerisque mauris.',
+		'catering': 'Catered',
+		'location': 'linktomap.com',
+	}
+	return render(request, 'halls/hallpage.html', context)
