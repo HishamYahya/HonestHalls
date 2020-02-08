cbbac53f2f12fefdc870c3c3e3a81fb7e89d8d3e
Hisham Alyahya <hishamaalyahya@gmail.com>
2020-02-08 17:35:40 +0000

    Added comments for filter

--- honesthalls/filter/forms.py
+++ honesthalls/filter/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class FilterForm(forms.Form):
+    # TODO: Add fields here
     isEnsuite = forms.BooleanField(label='Ensuite? ', required=False)
--- honesthalls/filter/views.py
+++ honesthalls/filter/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .forms import FilterForm
 
 
-# Create your views here.
 def filter_view(request):
     halls = [
         {
@@ -14,14 +13,27 @@ def filter_view(request):
             'isEnsuite': False
         }
     ]
+
+    # request.POST returns null if the form hasn't been submitted yet
+    # It's used to tell the template to show all halls
+    # the first time the page loads
     submitted = request.POST
+
+    # Initilize filters
+    # TODO: Add filter variables here 
     isEnsuite = None
+
     if(submitted):
         form = FilterForm(request.POST)
         if(form.is_valid()):
+            # Get boolean value of checkbox
+            # TODO: Assign here all the filter variables to
+            # their respective values in the form
             isEnsuite = form.cleaned_data.get('isEnsuite')
     else:
         form = FilterForm()
+
+    # Pass the halls data and the filters to the template
     context = {
         'form': form,
         'halls': halls,
