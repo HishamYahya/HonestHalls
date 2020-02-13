46dbd2384f454e6cd9dc788b732fc34f317a3230
Ayesha <ayesha.akhtar-3@student.manchester.ac.uk>
2020-02-13 17:36:01 +0000

    removing merge issues

--- honesthalls/halls/views.py
+++ honesthalls/halls/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import (
     Http404, HttpResponse, HttpResponseRedirect
 )
 from django.utils import timezone
-<<<<<<< HEAD
 from django.shortcuts import get_object_or_404, get_list_or_404
-from .models import Hall, RoomType, HallPhotos
+from .models import Hall, RoomType, HallPhotos, Review
 
 
 def index(request):
@@ -18,15 +17,6 @@ def index(request):
         'server_time': timezone.now(),
         'sample_halls': sample_halls
     }
-=======
-from .models import Review
-
-def index(request):
-    """ Serves the project homepage """
-
- 
-    context = {'server_time': timezone.now()}
->>>>>>> reviews
     if request.user.is_authenticated:
         context['username'] = request.user.username
 
@@ -38,19 +28,10 @@ def hallpage(request, id):
     roomtypes = hall.roomtype_set.all()
     hallphotos = hall.hallphotos_set.all()
     context = {
-<<<<<<< HEAD
         'id': id,
         'hall': hall,
         'roomtypes': roomtypes,
         'hallphotos': hallphotos,
-=======
-        'title': 'Hall Page',
-        'hallname': 'Hulme Hall',
-        'campus': 'Victoria Park',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at eros porta elit faucibus luctus. Maecenas justo massa, euismod in pulvinar in, accumsan a erat. Sed ac dui ipsum. Nulla dapibus viverra rutrum. Proin id felis at massa ornare tristique. Morbi turpis mauris, facilisis non dolor vel, gravida scelerisque mauris.',
-        'catering': 'Catered',
-        'location': 'linktomap.com',
         'reviews' : Review.objects.all()
->>>>>>> reviews
     }
     return render(request, 'halls/hallpage.html', context)
