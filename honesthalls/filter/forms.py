852bb4b8be8ed50a472a8bf99b5b63964b64f4b9
Ayesha <ayesha.akhtar-3@student.manchester.ac.uk>
2020-02-18 13:52:30 +0000

    changed filter

--- honesthalls/filter/forms.py
+++ honesthalls/filter/forms.py
@@ -1,10 +1,31 @@
 from django import forms
 
+# FRUIT_CHOICES= [
+#     ('orange', 'Oranges'),
+#     ('cantaloupe', 'Cantaloupes'),
+#     ('mango', 'Mangoes'),
+#     ('honeydew', 'Honeydews'),
+#     ]
+
+# class UserForm(forms.Form):
+#     first_name= forms.CharField(max_length=100)
+#     last_name= forms.CharField(max_length=100)
+#     email= forms.EmailField()
+#     age= forms.IntegerField()
+#     favorite_fruit= forms.CharField(label='What is your favorite fruit?', widget=forms.Select(choices=FRUIT_CHOICES))
+
+# key, value shown to user
+BASIN_ENSUITE_NA = [
+    ('ensuite', 'Ensuite'),
+    ('basin', 'Basin'),
+    ('neither', 'Neither')
+    ]
 
 class FilterForm(forms.Form):
     # TODO: Add fields here
     isEnsuite = forms.BooleanField(label='Ensuite ', required=False)
-    isCatered = forms.BooleanField(label='Catered ', required=False)
     hasBasin = forms.BooleanField(label='Basin ', required=False)
+    toilet_options = forms.ChoiceField(choices = BASIN_ENSUITE_NA)
+    isCatered = forms.BooleanField(label='Catered ', required=False)
     isSingle = forms.BooleanField(label='Single ', required=False)
     isDouble = forms.BooleanField(label='Double ', required=False)
--- honesthalls/filter/views.py
+++ honesthalls/filter/views.py
@@ -1,31 +1,34 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .forms import *
+from halls.models import Hall, RoomType
 
 
 def filter_view(request):
     # Dummy data currently being used
-    halls = [
-        {
-            'name': 'Hall1',
-            'isEnsuite': True,
-            'isCatered' : False,
-            'hasBasin' : False,
-            'isSingle' : True,
-            'isDouble' : False,
-            'price': 1000,
-            'Campus' : 'Fallowfield'
-        },
-        {
-            'name': 'Hall2',
-            'isEnsuite': False,
-            'isCatered' : True,
-            'hasBasin' : True,
-            'isSingle' : False,
-            'isDouble' : True,
-            'price': 2000,
-            'Campus' : 'Fallowfield'
-        }
-    ]
+    # halls = [
+    #     {
+    #         'name': 'Hall1',
+    #         'isEnsuite': True,
+    #         'isCatered' : False,
+    #         'hasBasin' : False,
+    #         'isSingle' : True,
+    #         'isDouble' : False,
+    #         'price': 1000,
+    #         'Campus' : 'Fallowfield'
+    #     },
+    #     {
+    #         'name': 'Hall2',
+    #         'isEnsuite': False,
+    #         'isCatered' : True,
+    #         'hasBasin' : True,
+    #         'isSingle' : False,
+    #         'isDouble' : True,
+    #         'price': 2000,
+    #         'Campus' : 'Fallowfield'
+    #     }
+    # ]
+    
 
     # request.POST returns null if the form hasn't been submitted yet
     # It's used to tell the template to show all halls
@@ -35,13 +38,22 @@ def filter_view(request):
     # Initilize filters
     # TODO: Add filter variables here
     search = None
-    isEnsuite = None
-    isCatered = None
-    hasBasin = None
-    isSingle = None
-    isDouble = None
-    price1, price2 = 0, 0
-    Campus = None
+    # isEnsuite = None
+    # isCatered = None
+    # hasBasin = None
+    # isSingle = None
+    # isDouble = None
+    # price1, price2 = 0, 0
+    # Campus = None
+
+    #^ do we need this???
+
+    #results_halls = results_rooms.distinct('hall_id')
+    #results_halls = Hall.objects.all()
+    results_rooms = RoomType.objects.all()
+    unique_halls = set()
+
+
     if(submitted):
         form = FilterForm(request.POST)
         if(form.is_valid()):
@@ -49,38 +61,95 @@ def filter_view(request):
             # TODO: Assign here all the filter variables to
             # their respective values in the form
             search = submitted.get('search')
-            isEnsuite = form.cleaned_data.get('isEnsuite')
             isCatered = form.cleaned_data.get('isCatered')
+            isEnsuite = form.cleaned_data.get('isEnsuite')
             hasBasin = form.cleaned_data.get('hasBasin')
-            isSingle = form.cleaned_data.get('isSingle')
-            isDouble = form.cleaned_data.get('isDouble')
+            basin_ensuite = form.cleaned_data.get('toilet_options')
+            print(basin_ensuite)
+            isSingle = form.cleaned_data.get('isSingle') #???
+            isDouble = form.cleaned_data.get('isDouble') #???
             whichHall = form.cleaned_data.get('whichHall')
-            price1 = submitted.get('price1')
-            price2 = submitted.get('price2')
+            min_price = submitted.get('price1')
+            max_price = submitted.get('price2')
             Campus = submitted.get('Campus')
+
+            # tries need to be seperate to allow only min price or only max price to be used
             try:
-                price1 = int(price1)
-                price2 = int(price2)
+                min_price = int(min_price) * 100
             except:
-                price1 = 0
-                price2 = 0
+                min_price = None # was 0 before?
+
+            try:
+                max_price = int(max_price) * 100
+            except:
+                max_price = None # was 0 before?
+
+
+            # alternative bedsizes??
+            if (isSingle):
+                bedsize = "Single"
+            elif (isDouble):
+                bedsize = "Double"
+            else:
+                bedsize = None
+
+
+            # we use this to build up a query
+            # query = [Q(ensuite=isEnsuite), Q(catered=isCatered), Q(basin=hasBasin), Q(bedsize__iexact=bedsize)]
+            query = []
+            query.append(Q(ensuite=isEnsuite))
+            query.append(Q(catered=isCatered))
+            query.append(Q(basin=hasBasin))
+            query.append(Q(bedsize__iexact=bedsize))
+            if min_price == None and max_price == None: # if both fields left empty
+                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize)
+                #pass
+            elif max_price == None:
+                #query.append(Q(price__gte=min_price))
+                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize, price__gte=min_price)
+            elif min_price == None:
+                #query.append(Q(price__lte=max_price))
+                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize, price__lte=max_price)
+            elif min_price >= 0 and max_price >= 0 and min_price <= max_price: # if fields filled correctly
+                #query.append(Q(price__range=(min_price, max_price)))
+                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize, price__range=(min_price, max_price))
+            else:
+                #pass
+                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize)
+                # TODO: need to add an error for
+                #         - price1 > price2
+                #         - if either are less than 0
+
+            #results_rooms = RoomType.objects.filter(*query)
+
+            for i in results_rooms:
+                unique_halls.add(i.hall)
+
+                
 
     else:
         form = FilterForm()
+        for i in results_rooms:
+            unique_halls.add(i.hall)
+        print(unique_halls)
+
+
+    
 
     # Pass the halls data and the filters to the template form.html
     context = {
         'form': form,
-        'halls': halls,
+        'results_rooms': results_rooms,
+        'results_halls': unique_halls,
         'search': search,
-        'isEnsuite': isEnsuite,
-        'isCatered': isCatered,
-        'hasBasin' : hasBasin,
-        'isSingle' : isSingle,
-        'isDouble': isDouble,
-        'submitted': submitted,
-        'price1': price1,
-        'price2': price2,
-        'Campus': Campus
+        # 'isEnsuite': isEnsuite,
+        # 'isCatered': isCatered,
+        # 'hasBasin' : hasBasin,
+        # 'isSingle' : isSingle,
+        # 'isDouble': isDouble,
+        # 'submitted': submitted,
+        # 'price1': price1,
+        # 'price2': price2,
+        # 'Campus': Campus
     }
     return render(request, 'filter/form.html', context)
