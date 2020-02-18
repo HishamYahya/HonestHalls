8e73d1284d499b9f323f8da8d96d43b138339da3
Finlay Marsh <finlay_marsh@Finlays-MacBook-Pro.local>
2020-02-18 18:55:28 +0000

    added email into settings.py

--- honesthalls/honesthalls/settings.py
+++ honesthalls/honesthalls/settings.py
@@ -154,3 +154,11 @@ LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# -- don't forget to hide in environment variables when deploying!!! --
+EMAIL_HOST_USER = 'honesthalls@gmail.com'
+EMAIL_HOST_PASSWORD = 'F1rstPr0ject!'
