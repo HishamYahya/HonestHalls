5da54a1eb60dbe94a8e3fd524aa81220467f0454
Ayesha <ayesha.akhtar-3@student.manchester.ac.uk>
2020-02-18 14:24:22 +0000

    changed to drop downs

--- honesthalls/filter/forms.py
+++ honesthalls/filter/forms.py
@@ -1,31 +1,32 @@
 from django import forms
 
-# FRUIT_CHOICES= [
-#     ('orange', 'Oranges'),
-#     ('cantaloupe', 'Cantaloupes'),
-#     ('mango', 'Mangoes'),
-#     ('honeydew', 'Honeydews'),
-#     ]
-
-# class UserForm(forms.Form):
-#     first_name= forms.CharField(max_length=100)
-#     last_name= forms.CharField(max_length=100)
-#     email= forms.EmailField()
-#     age= forms.IntegerField()
-#     favorite_fruit= forms.CharField(label='What is your favorite fruit?', widget=forms.Select(choices=FRUIT_CHOICES))
+BEDSIZE_OPTIONS = [
+    ('na', 'Don\'t care'),
+    ('single', 'Single'),
+    ('double', 'Double')
+]
 
 # key, value shown to user
 BASIN_ENSUITE_NA = [
+    ('na', 'Don\'t care'),
+    ('neither', 'Neither'),
     ('ensuite', 'Ensuite'),
-    ('basin', 'Basin'),
-    ('neither', 'Neither')
+    ('basin', 'Basin')
     ]
+
+CATERED = [
+    ('na', 'Don\'t care'),
+    ('self', 'Self-catered'),
+    ('catered', 'Catered')
+]
 
 class FilterForm(forms.Form):
     # TODO: Add fields here
-    isEnsuite = forms.BooleanField(label='Ensuite ', required=False)
-    hasBasin = forms.BooleanField(label='Basin ', required=False)
+    #isEnsuite = forms.BooleanField(label='Ensuite ', required=False)
+    #hasBasin = forms.BooleanField(label='Basin ', required=False)
     toilet_options = forms.ChoiceField(choices = BASIN_ENSUITE_NA)
-    isCatered = forms.BooleanField(label='Catered ', required=False)
-    isSingle = forms.BooleanField(label='Single ', required=False)
-    isDouble = forms.BooleanField(label='Double ', required=False)
+    bed_options = forms.ChoiceField(choices = BEDSIZE_OPTIONS)
+    eat_options = forms.ChoiceField(choices = CATERED)
+    #isCatered = forms.BooleanField(label='Catered ', required=False)
+    #isSingle = forms.BooleanField(label='Single ', required=False)
+    #isDouble = forms.BooleanField(label='Double ', required=False)
--- honesthalls/filter/views.py
+++ honesthalls/filter/views.py
@@ -61,13 +61,23 @@ def filter_view(request):
             # TODO: Assign here all the filter variables to
             # their respective values in the form
             search = submitted.get('search')
-            isCatered = form.cleaned_data.get('isCatered')
-            isEnsuite = form.cleaned_data.get('isEnsuite')
-            hasBasin = form.cleaned_data.get('hasBasin')
+
+
+            #isCatered = form.cleaned_data.get('isCatered')
+
+            catered = form.cleaned_data.get('eat_options')
+
+            #isEnsuite = form.cleaned_data.get('isEnsuite')
+            #hasBasin = form.cleaned_data.get('hasBasin')
             basin_ensuite = form.cleaned_data.get('toilet_options')
+
             print(basin_ensuite)
-            isSingle = form.cleaned_data.get('isSingle') #???
-            isDouble = form.cleaned_data.get('isDouble') #???
+
+            #isSingle = form.cleaned_data.get('isSingle') #???
+            #isDouble = form.cleaned_data.get('isDouble') #???
+
+            bedsize = form.cleaned_data.get('bed_options')
+
             whichHall = form.cleaned_data.get('whichHall')
             min_price = submitted.get('price1')
             max_price = submitted.get('price2')
@@ -85,42 +95,53 @@ def filter_view(request):
                 max_price = None # was 0 before?
 
 
-            # alternative bedsizes??
-            if (isSingle):
-                bedsize = "Single"
-            elif (isDouble):
-                bedsize = "Double"
-            else:
-                bedsize = None
-
-
             # we use this to build up a query
             # query = [Q(ensuite=isEnsuite), Q(catered=isCatered), Q(basin=hasBasin), Q(bedsize__iexact=bedsize)]
             query = []
-            query.append(Q(ensuite=isEnsuite))
-            query.append(Q(catered=isCatered))
-            query.append(Q(basin=hasBasin))
-            query.append(Q(bedsize__iexact=bedsize))
+            
+            #query.append(Q(catered=isCatered))
+            
+            #query.append(Q(bedsize__iexact=bedsize))
+
+            if basin_ensuite == 'basin':
+                query.append(Q(basin=True))
+            elif basin_ensuite == 'ensuite':
+                query.append(Q(ensuite=True))
+            elif basin_ensuite == 'neither':
+                query.append(Q(basin=False))
+                query.append(Q(ensuite=False))
+            
+
+            if bedsize == 'single':
+                query.append(Q(bedsize__iexact='Single'))
+            elif bedsize == 'double':
+                query.append(Q(bedsize__iexact='Double'))
+
+            if catered == 'self':
+                query.append(Q(catered=False))
+            elif catered == 'catered':
+                query.append(Q(catered=True))
+
             if min_price == None and max_price == None: # if both fields left empty
-                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize)
-                #pass
+                #results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize)
+                pass
             elif max_price == None:
-                #query.append(Q(price__gte=min_price))
-                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize, price__gte=min_price)
+                query.append(Q(price__gte=min_price))
+                #results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize, price__gte=min_price)
             elif min_price == None:
-                #query.append(Q(price__lte=max_price))
-                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize, price__lte=max_price)
+                query.append(Q(price__lte=max_price))
+                #results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize, price__lte=max_price)
             elif min_price >= 0 and max_price >= 0 and min_price <= max_price: # if fields filled correctly
-                #query.append(Q(price__range=(min_price, max_price)))
-                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize, price__range=(min_price, max_price))
+                query.append(Q(price__range=(min_price, max_price)))
+                #results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize, price__range=(min_price, max_price))
             else:
-                #pass
-                results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize)
+                pass
+                #results_rooms = RoomType.objects.filter(ensuite=isEnsuite, catered=isCatered, basin=hasBasin, bedsize__iexact=bedsize)
                 # TODO: need to add an error for
                 #         - price1 > price2
                 #         - if either are less than 0
 
-            #results_rooms = RoomType.objects.filter(*query)
+            results_rooms = RoomType.objects.filter(*query)
 
             for i in results_rooms:
                 unique_halls.add(i.hall)
