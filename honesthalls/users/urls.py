d895d2c501838c72acb43e76aa0c77a4551747e4
Lea <lea.messaouddegalzain@student.manchester.ac.uk>
2020-02-20 17:34:51 +0000

    password reset done

--- honesthalls/users/urls.py
+++ honesthalls/users/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('verify/', user_views.verify, name='verify'),
     path('verify-complete/<uidb64>/<token>/', user_views.verify_complete, name='verify-complete'),
-    path('', user_views.profile, name='profile')
+    path('', user_views.profile, name='profile'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
